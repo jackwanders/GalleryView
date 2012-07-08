59c57c5c687c9b2c8eeae585d08a3b4541c795e7
joeletherton <joel.etherton@gmail.com>
2012-07-08 00:11:05 -0300

    Added randomness and applicable option to filmstrip showNext when "playing".

--- js/jquery.galleryview-3.0-dev.js
+++ js/jquery.galleryview-3.0-dev.js
@@ -536,7 +536,15 @@ if (typeof Object.create !== 'function') {
 		
 		showNext: function() {
 			this.navAction = 'next';
-			this.showItem(this.frameIterator+1);
+            		if (this.playing && this.opts.play_random_order) {
+		                var nextImage = this.frameIterator;
+                		while (nextImage == this.frameIterator) {
+                    			nextImage = Math.floor(Math.random()*(this.numImages + 1));
+                		}
+               			this.showItem(nextImage);
+            		} else {
+	            		this.showItem(this.frameIterator + 1);
+            		}
 		},
 		
 		showPrev: function() {
@@ -1082,38 +1090,39 @@ if (typeof Object.create !== 'function') {
 		easing: 'swing', 				//STRING - easing method to use for animations (jQuery provides 'swing' or 'linear', more available with jQuery UI or Easing plugin)
 		
 		// Panel Options
-		show_panels: true, 				//BOOLEAN - flag to show or hide panel portion of gallery
+		show_panels: true, 			//BOOLEAN - flag to show or hide panel portion of gallery
 		show_panel_nav: true, 			//BOOLEAN - flag to show or hide panel navigation buttons
-		enable_overlays: false, 			//BOOLEAN - flag to show or hide panel overlays
-		panel_width: 800, 				//INT - width of gallery panel (in pixels)
-		panel_height: 400, 				//INT - height of gallery panel (in pixels)
+		enable_overlays: false, 		//BOOLEAN - flag to show or hide panel overlays
+		panel_width: 800, 			//INT - width of gallery panel (in pixels)
+		panel_height: 400, 			//INT - height of gallery panel (in pixels)
 		panel_animation: 'fade', 		//STRING - animation method for panel transitions (crossfade,fade,slide,none)
 		panel_scale: 'crop', 			//STRING - cropping option for panel images (crop = scale image and fit to aspect ratio determined by panel_width and panel_height, fit = scale image and preserve original aspect ratio)
-		overlay_position: 'bottom', 	//STRING - position of panel overlay (bottom, top)
-		pan_images: false,				//BOOLEAN - flag to allow user to grab/drag oversized images within gallery
-		pan_style: 'drag',				//STRING - panning method (drag = user clicks and drags image to pan, track = image automatically pans based on mouse position
-		pan_smoothness: 15,				//INT - determines smoothness of tracking pan animation (higher number = smoother)
-                link_newwindow: true,           //BOOLEAN - flag to open clickable images in a new window
-                panel_clickable: false,         //BOOLEAN - flag to determine if the panel is clickable
+		overlay_position: 'bottom', 		//STRING - position of panel overlay (bottom, top)
+		pan_images: false,			//BOOLEAN - flag to allow user to grab/drag oversized images within gallery
+		pan_style: 'drag',			//STRING - panning method (drag = user clicks and drags image to pan, track = image automatically pans based on mouse position
+		pan_smoothness: 15,			//INT - determines smoothness of tracking pan animation (higher number = smoother)
+                link_newwindow: true,           	//BOOLEAN - flag to open clickable images in a new window
+                panel_clickable: false,        		//BOOLEAN - flag to determine if the panel is clickable
         
 		// Filmstrip Options
-		start_frame: 1, 				//INT - index of panel/frame to show first when gallery loads
+		start_frame: 1, 			//INT - index of panel/frame to show first when gallery loads
 		show_filmstrip: true, 			//BOOLEAN - flag to show or hide filmstrip portion of gallery
 		show_filmstrip_nav: true, 		//BOOLEAN - flag indicating whether to display navigation buttons
 		enable_slideshow: true,			//BOOLEAN - flag indicating whether to display slideshow play/pause button
-		autoplay: false,				//BOOLEAN - flag to start slideshow on gallery load
+		autoplay: false,			//BOOLEAN - flag to start slideshow on gallery load
+       		play_random_order: false,       	//BOOLEAN - while playing the images are "playing" use a random order
 		show_captions: false, 			//BOOLEAN - flag to show or hide frame captions	
-		filmstrip_size: 3, 				//INT - number of frames to show in filmstrip-only gallery
+		filmstrip_size: 3, 			//INT - number of frames to show in filmstrip-only gallery
 		filmstrip_style: 'scroll', 		//STRING - type of filmstrip to use (scroll = display one line of frames, scroll filmstrip if necessary, showall = display multiple rows of frames if necessary)
-		filmstrip_position: 'bottom', 	//STRING - position of filmstrip within gallery (bottom, top, left, right)
-		frame_width: 80, 				//INT - width of filmstrip frames (in pixels)
-		frame_height: 40, 				//INT - width of filmstrip frames (in pixels)
+		filmstrip_position: 'bottom', 		//STRING - position of filmstrip within gallery (bottom, top, left, right)
+		frame_width: 80, 			//INT - width of filmstrip frames (in pixels)
+		frame_height: 40, 			//INT - width of filmstrip frames (in pixels)
 		frame_opacity: 0.4, 			//FLOAT - transparency of non-active frames (1.0 = opaque, 0.0 = transparent)
 		frame_scale: 'crop', 			//STRING - cropping option for filmstrip images (same as above)
-		frame_gap: 5, 					//INT - spacing between frames within filmstrip (in pixels)
+		frame_gap: 5, 				//INT - spacing between frames within filmstrip (in pixels)
 		
 		// Info Bar Options
-		show_infobar: true,				//BOOLEAN - flag to show or hide infobar
-		infobar_opacity: 1				//FLOAT - transparency for info bar
+		show_infobar: true,			//BOOLEAN - flag to show or hide infobar
+		infobar_opacity: 1			//FLOAT - transparency for info bar
 	};
 })(jQuery);
